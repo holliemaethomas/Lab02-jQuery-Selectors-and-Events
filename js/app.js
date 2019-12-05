2552d8990524b6e7f2209de4f6249f5d5cc2931f
holliemaethomas <holliemaethomas85@gmail.com>
2019-12-04 16:23:24 -0800

    changed render

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 'use strict';
+
 let hornKeywords = [];
-// Jquery notes
+
 
 // getter
 let main = $('main');
 
 // setting element content
-
 $('h1').text('The Gallery of Horns');
-
 $('select').attr('id', 'dropBox')
 $('select').append('option')
 
 
-// setting html elements
+// setting the static html elements
 main.append(`
 <h1></h1>
 <select></select>
 `)
+
 // This function gets the keywords out of JSON file and appends them to the dropdown as options
 $.get('../data/page-1.json').then(
   (dataArray) => {
@@ -33,18 +33,18 @@ $('h1').text('The Gallery of Horns');
 $('select').attr('id', 'dropBox')
 
 
-
+// constructor function to assign props of JSON objs
 let HornObjects = [];
 function HornObject(image_url, title, description, keyword, horns) {
   this.image_url = image_url
   this.title = title
   this.description = description
   this.keyword = keyword
-  this.horns = 0
+  this.horns = horns
 }
 
 
-// This function gets the objects a whole from JSON
+// This function gets the objects as a whole from JSON
 let allObjects = [];
 $.get('../data/page-1.json').then(
   (dataArray) => {
@@ -55,25 +55,25 @@ $.get('../data/page-1.json').then(
       new HornObject(allObjects[j])
     }
   });
-console.log(allObjects);
 
 
 
-const showImage = function () {
 
-  allObjects.forEach(
-    $('#display').append(`
-    <div>
-      <h2>${allObjects.title}</h2>
-      <img src="${allObjects.image_url}"></img>
-      <p>${allObjects.description}</p>
-      <p>${allObjects.keyword}</p>
-      <p>${allObjects.horns}</p>
-    </div>
-  `))
+HornObject.prototype.renderWithJquery = function () {
+  let clone = $('#display').clone();
+  clone.find('img').attr('src', this.image_url)
+  clone.find('h2').text(this.title)
+  clone.find('#description').text(this.description)
+  clone.find('#horn').text(this.horns)
+  clone.find('#keyword').text(this.description)
+  $('#display').append(clone)
+  console.log(clone);
 }
 
 
-showImage();
+HornObject.prototype.render()
 
+allObjects.forEach(HornObject => {
+  HornObject.renderWithJquery()
+})
 
