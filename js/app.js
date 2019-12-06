713c05b2d705bdd669948a9e87dad8ccab692bac
holliemaethomas <holliemaethomas85@gmail.com>
2019-12-05 22:13:57 -0800

    not working, dont need to merge

--- js/app.js
+++ js/app.js
@@ -1,87 +1,87 @@
 'use strict'
 
-function SpikedImages(item) {
-  this.image_url = item.image_url;
-  this.title = item.title;
-  this.horns = item.horns;
-  this.keyword = item.keyword;
-  this.description = item.description;
+// function SpikedImages(item) {
+//   this.image_url = item.image_url;
+//   this.title = item.title;
+//   this.horns = item.horns;
+//   this.keyword = item.keyword;
+//   this.description = item.description;
 
-  imageTotals.push(this);
-}
+//   imageTotals.push(this);
+// }
 
-const imageTotals = [];
+// const imageTotals = [];
 
-SpikedImages.prototype.renderHornItem = function () {
-  $('main').append('<section class="clone"><section>');
-  let $clone = $('section[class="clone"]');
-  let cloneTemplate = $('#photo-template').html();
+// SpikedImages.prototype.renderHornItem = function () {
+//   $('main').append('<section class="clone"><section>');
+//   let $clone = $('section[class="clone"]');
+//   let cloneTemplate = $('#photo-template').html();
 
-  $clone.html(cloneTemplate);
+//   $clone.html(cloneTemplate);
 
-  $clone.find('h2').text(this.title);
-  $clone.find('p').text(this.description);
-  $clone.find('img').attr('src', this.image_url);
-  $clone.find('img').attr('alt', this.title);
-  $clone.removeClass('clone');
-  $clone.attr('class', this.keyword);
-}
+//   $clone.find('h2').text(this.title);
+//   $clone.find('p').text(this.description);
+//   $clone.find('img').attr('src', this.image_url);
+//   $clone.find('img').attr('alt', this.title);
+//   $clone.removeClass('clone');
+//   $clone.attr('class', this.keyword);
+// }
 
-Image.prototype.renderWithHandlerBars = function () {
-  let spikeHtml = $('#photo-template').html();
-  const renderImageWithHandlebars = Handlebars.compile(spikeHtml);
-  const spikeImage = renderImageWithHandlebars(this);
-  $('main').append(hornImage);
-};
+// Image.prototype.renderWithHandlerBars = function () {
+//   let spikeHtml = $('#photo-template').html();
+//   const renderImageWithHandlebars = Handlebars.compile(spikeHtml);
+//   const spikeImage = renderImageWithHandlebars(this);
+//   $('main').append(hornImage);
+// };
 
 
-SpikedImages.prototype.renderKeyValues = function () {
-  let filterSelectValues = [];
-  // remove all elements except for first one
-  $('option').not(':first').remove();
-  imageTotals.forEach(image => {
-    if (!filterSelectValues.includes(image.keyword)) {
-      filterSelectValues.push(image.keyword);
-    }
-  });
+// SpikedImages.prototype.renderKeyValues = function () {
+//   let filterSelectValues = [];
+//   // remove all elements except for first one
+//   $('option').not(':first').remove();
+//   imageTotals.forEach(image => {
+//     if (!filterSelectValues.includes(image.keyword)) {
+//       filterSelectValues.push(image.keyword);
+//     }
+//   });
 
-  filterSelectValues.sort();
+//   filterSelectValues.sort();
 
-  filterSelectValues.forEach(keyword => {
-    let altPhrase = `<option value="${keyword}">${keyword}</option>`;
-    $('select').append(altPhrase);
-  });
-}
+//   filterSelectValues.forEach(keyword => {
+//     let altPhrase = `<option value="${keyword}">${keyword}</option>`;
+//     $('select').append(altPhrase);
+//   });
+// }
 
-function filterSpikePics() {
-  $('select').on('change', function () {
-    let passPhrase = $(this).val();
-    if (passPhrase !== 'default') {
-      $('section').hide();
-      $(`section[class = "${passPhrase}"]`).show();
-    } else {
-      $('section').show();
-    }
-  });
-}
+// function filterSpikePics() {
+//   $('select').on('change', function () {
+//     let passPhrase = $(this).val();
+//     if (passPhrase !== 'default') {
+//       $('section').hide();
+//       $(`section[class = "${passPhrase}"]`).show();
+//     } else {
+//       $('section').show();
+//     }
+//   });
+// }
 
-function jsonData() {
-  $.get('./data/page-1.json', 'json')
-    .then(data => {
-      data.forEach(spikedImageObj => {
-        new SpikedImages(spikedImageObj);
-      })
-    })
-    .then(() => {
-      imageTotals.forEach(image => {
-        image.renderHornItem();
-        image.renderKeyValues();
-      })
-    })
-}
+// function jsonData() {
+//   $.get('./data/page-1.json', 'json')
+//     .then(data => {
+//       data.forEach(spikedImageObj => {
+//         new SpikedImages(spikedImageObj);
+//       })
+//     })
+//     .then(() => {
+//       imageTotals.forEach(image => {
+//         image.renderHornItem();
+//         image.renderKeyValues();
+//       })
+//     })
+// }
 
-$(() => {
-  jsonData();
-  filterSpikePics();
-});
+// $(() => {
+//   jsonData();
+//   filterSpikePics();
+// });
 
--- js/handleBars.js
+++ js/handleBars.js
@@ -1,81 +1,85 @@
-/* eslint-disable no-undef */
 'use strict'
 
-let allSpikedImages = [];
-let horns = ['Filter by Horns'];
-let keywordArr = ['Filter by Keyword'];
-let sortBy = ['Sort by', 'title', 'horns'];
-let finalSort = [];
-let idGetter = ['#section-template', '#option-template'];
-let options = [['.FilterByKeyword', keywordArr], ['.imgFilter', '.FilterbyHorns'], ['.sortFilter', sortBy]];
+var spikeImageArr = [];
+var keywordArr = ['Filter by Keyword'];
+var hornsArr = ['Filter by Horns'];
+var sortBy = ['Sort by', 'title', 'horns'];
+var finalSort = [];
+var idGetter = ['#section-template', '#option-template'];
+var options = [
+  ['.keywordFilter', keywordArr],
+  ['.imgFilter', hornsArr],
+  ['.sortFilter', sortBy]
+];
 
-const SpikedImages = function (image_url, title, description, keyword, horns) {
+var SpikedImages = function(image_url, title, description, keyword, horns) {
   this.image_url = image_url;
   this.title = title;
   this.description = description;
   this.keyword = keyword
   this.horns = horns
-  allSpikedImages.push(this)
+  spikeImageArr.push(this)
 };
 
 SpikedImages.prototype.renderSpikes = function () {
-  htmlSetter(idGetter[0], '.content-placeHolder', allSpikedImages)
+  htmlSetter(idGetter[0], '.content-placeHolder', spikeImageArr)
 };
 
- let htmlSetter = (templateID, htmlClass, arr) => {
-  let templateScript = $(templateID).html();
-  let template = Handlebars.compile(templateScript);
-  let compiledHTML = template(arr);
+var htmlSetter = (templateId, htmlClass, arr) => {
+  var templateScript = $(templateId).html();
+  var template = Handlebars.compile(templateScript);
+  var compiledHTML = template(arr);
   $(htmlClass).html(compiledHTML);
-}
+};
 
 
-const sorted = selection => {
-  finalSort = allSpikedImages.sort((a, b) => {
+var sorted = selection => {
+  finalSort = spikeImageArr.sort((a, b) => {
     if (a[selection] < b[selection]) return -1;
-    if (a[selection] > b[selection]) return -1;
+    if (a[selection] > b[selection]) return 1;
     return 0;
   });
   finalSort.forEach(titleSort => titleSort.renderSpikes)
 }
 
 
-let keywords = ['Filter By Keyword']
-const buildSpiked = imageJSON => {
+// var keywords = ['Filter By Keyword']
+var buildSpiked = imageJSON => {
   imageJSON.forEach(spikeImage => {
     new SpikedImages(spikeImage.image_url, spikeImage.title, spikeImage.description, spikeImage.keyword, spikeImage.horns);
-    if (!keywords.includes(spikeImage.keyword)) {
-      keyword.push(spikeImage.keyword);
+    if (!keywordArr.includes(spikeImage.keyword)){
+      keywordArr.push(spikeImage.keyword);
     }
-    if (!keywords.includes(spikeImage.horns)) {
-      keyword.push(spikeImage.horns);
+    if (!hornsArr.includes(spikeImage.horns)) {
+      hornsArr.push(spikeImage.horns);
     }
-  }); options.forEach(value => htmlSetter(idGetter[1], value[0], value[1]));
-  allSpikedImages.forEach(spikeImage.renderSpikes);
+  });
+  options.forEach(value => htmlSetter(idGetter[1], value[0], value[1]));
+  spikeImageArr.forEach(spikeImage => spikeImage.renderSpikes);
 }
 
-const hideIt = () => {
+var hideIt = () => {
   $('section').hide();
   $('h2').hide();
   $('img').hide();
   $('p').hide();
 }
 
-const showIt = label => {
-  $(`section ${label} `).show();
-  $(`h2 ${label} `).show();
-  $(`img ${label} `).show();
-  $(`p ${label} `).show();
+var showIt = label => {
+  $(`section${label}`).show();
+  $(`h2${label}`).show();
+  $(`img${label}`).show();
+  $(`p${label}`).show();
 }
 
-const defaulter = (drop1, drop2) => {
+var defaulter = (drop1, drop2) => {
   $(options[drop1])[0].selectedIndex = 'default';
   $(options[drop2])[0].selectedIndex = 'default';
 };
 
 // keyword filter
 $('select[class = "FilterByKeyWord"]').on('change', function () {
-  let $select = $(this).val();
+  var $select = $(this).val();
   hideIt();
   showIt(`[data=$"${$select}"]`);
   defaulter(0, 2);
@@ -83,19 +87,19 @@ $('select[class = "FilterByKeyWord"]').on('change', function () {
 
 //  spiked image filter
 $('select[class=".imgFilter"]').on('change', function () {
-  let $select = $(this).val();
+  var $select = $(this).val();
   hideIt();
   showIt(`[data=$"${$select}"]`);
   defaulter(0, 2);
 });
 
 $('select[class="sortFilter"]').on('change', function () {
-  let $select = $(this).val()
+  var $select = $(this).val()
   $('main').empty();
   sorted($select);
   $('section').show();
   $('h2').show();
-  $(img).show();
+  $('img').show();
   $('p').show();
   defaulter[0, 2]
 });
@@ -105,21 +109,25 @@ $('button[name="reset"]').click(function () {
 });
 
 SpikedImages.getAll = (fileName) => {
-  let filePath = `${fileName}`;
-  let fileType = 'json';
+  var filePath = `${fileName}`;
+  var fileType = 'json';
   $.get(filePath, fileType).then(buildSpiked)
 };
 
-$('button[class="page1"]').click(function () {
-  SpikedImages.length = 0;
+$('button[class="page-1"]').click(function() {
+  spikeImageArr.length = 0;
   $('main').empty();
-  Images.getAllImagesFromFile('data/page-1.json');
+  SpikedImages.getAll('data/page-1.json');
 });
 
-$('button[class="page1"]').click(function () {
-  SpikedImages.length = 0;
+$('button[class="page-2"]').click(function() {
+  (spikeImageArr.length = 0);
   $('main').empty();
-  Images.getAllImagesFromFile('data/page-1.json');
+  SpikedImages.getAll('data/page-2.json');
 });
+
+
+
 
 // nick Paro helped me redo this lab
+
